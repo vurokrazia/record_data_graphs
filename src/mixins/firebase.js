c6918b7d7d1d883b3a8da86573f205c479af5358
Jesus Martinez <jesus.alberto.vk@gmail.com>
2020-01-26 16:13:29 -0600

    ...

--- src/mixins/firebase.js
+++ src/mixins/firebase.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "B"
+  apiKey: "AIzaSyDfr6euH8QwydG-jdTaoRRIClstMiBOXBI",
+  authDomain: "flutter-push-veco.firebaseapp.com",
+  databaseURL: "https://flutter-push-veco.firebaseio.com",
+  projectId: "flutter-push-veco",
+  storageBucket: "flutter-push-veco.appspot.com",
+  messagingSenderId: "460194401729",
+  appId: "1:460194401729:web:eb62bee06440347ec37340",
+  measurementId: "G-EPR7Q8316B"
 };
